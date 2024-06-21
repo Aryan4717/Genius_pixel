27bd8626f3b02dc1049d03d9c4e93ace58ec1bf3
Aryan4717 <ak1710650@gmail.com>
2024-06-21 23:28:50 +0530

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,29 +1,10 @@
-"use client"
-
-// pages/sign-in/[[...sign-in]].js
-
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { SignIn } from '@clerk/nextjs';
+import { SignIn } from '@clerk/nextjs'
 
 const SignInPage = () => {
-  const router = useRouter();
+  return <SignIn />
+}
 
-  useEffect(() => {
-    // Optional: Redirect users if they visit `/sign-in` directly
-    router.replace('/');
-  }, []);
+export default SignInPage
 
-  return <SignIn />;
-};
 
-export default SignInPage;
 
-// export async function getStaticPaths() {
-//   // Since it's a sign-in page, you might not have multiple variations to pre-render statically
-//   // If you do have specific paths you want to pre-render, define them here
-//   return {
-//     paths: [],
-//     fallback: false, // Show 404 if the page doesn't match any path
-//   };
-// }
