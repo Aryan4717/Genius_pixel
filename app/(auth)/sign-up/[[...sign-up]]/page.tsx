1fc0ffac777692642ad44eadf3f097dcc5f5242c
Aryan4717 <ak1710650@gmail.com>
2024-06-21 23:29:32 +0530

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,29 +1,7 @@
-"use client"
-
-// pages/sign-up/[[...sign-up]].js
-
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { SignUp } from '@clerk/nextjs';
+import { SignUp } from '@clerk/nextjs'
 
 const SignUpPage = () => {
-  const router = useRouter();
+  return <SignUp />
+}
 
-  useEffect(() => {
-    // Optional: Redirect users if they visit `/sign-up` directly
-    router.replace('/');
-  }, []);
-
-  return <SignUp />;
-};
-
-export default SignUpPage;
-
-// export async function getStaticPaths() {
-//   // Since it's a sign-up page, you might not have multiple variations to pre-render statically
-//   // If you do have specific paths you want to pre-render, define them here
-//   return {
-//     paths: [],
-//     fallback: false, // Show 404 if the page doesn't match any path
-//   };
-// }
+export default SignUpPage
